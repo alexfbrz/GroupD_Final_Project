6dbe87a103e146a88891a13b09fbceaec4231673
Kendra Clay <kclay618@gmail.com>
2018-11-12 21:39:02 -0500

    11/12 update

--- app/src/main/java/com/example/kendra/groupd_finalproject/GameActivity.java
+++ app/src/main/java/com/example/kendra/groupd_finalproject/GameActivity.java
@@ -1,13 +1,70 @@
 package com.example.kendra.groupd_finalproject;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class GameActivity extends Activity {
+
+    TextView usernameTV;
+    TextView livesTV;
+    TextView scoreTV;
+    TextView lvlTV;
+    RelativeLayout gameSpace;
+
+    CountDownTimer timer;
+
+    Random randNum;
+
+    ImageView image;
+
+    Intent myIntent2;
+    Intent myIntent3;
+
+    int lives;
+    int score;
+    int xPos =1;
+    int imgInd =0;
+
+    int[] imgArr ={R.drawable.hearticon, R.drawable.dino};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        randNum = new Random();
+
+        usernameTV = findViewById(R.id.userNameTV);
+        livesTV = findViewById(R.id.livesTV);
+        scoreTV = findViewById(R.id.scoreTV);
+        lvlTV = findViewById(R.id.lvlTV);
+        gameSpace = findViewById(R.id.gameSpace);
+
+        myIntent2 = getIntent();
+        String username = myIntent2.getStringExtra("username");
+        usernameTV.setText(username);
+        
     }
+
+
+    @Override
+    public void onBackPressed() {
+    }
+
+
 }
--- app/src/main/java/com/example/kendra/groupd_finalproject/LoginActivity.java
+++ app/src/main/java/com/example/kendra/groupd_finalproject/LoginActivity.java
@@ -1,13 +1,52 @@
 package com.example.kendra.groupd_finalproject;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
+
+    TextView error;
+    EditText unTV;
+    Button playBtn;
+    String errorStr = "Enter a username to continue";
+    String username;
+    Intent myIntent1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        error = findViewById(R.id.error);
+        unTV = findViewById(R.id.unTV);
+        playBtn = findViewById(R.id.playBtn);
+
+        error.setVisibility(View.GONE);
+
+        myIntent1 = new Intent(this, GameActivity.class);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = unTV.getText().toString();
+                if (username.equals(""))
+                {
+                    error.setText(errorStr);
+                    error.setVisibility(View.VISIBLE);
+                }
+                else {
+                    error.setVisibility(View.GONE);
+                    myIntent1.putExtra("username", username);
+                    LoginActivity.this.startActivity(myIntent1);
+                }
+            }
+        });
+
+
     }
 }
