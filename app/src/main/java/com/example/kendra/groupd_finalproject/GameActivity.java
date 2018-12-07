99fb2573d7828163f829b90666a9ee5d1b5eb77d
Kendra Clay <kclay618@gmail.com>
2018-12-07 08:35:46 -0500

    12/7

--- app/src/main/java/com/example/kendra/groupd_finalproject/GameActivity.java
+++ app/src/main/java/com/example/kendra/groupd_finalproject/GameActivity.java
@@ -3,9 +3,7 @@ package com.example.kendra.groupd_finalproject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.CountDownTimer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -30,10 +28,12 @@ public class GameActivity extends Activity {
     Intent myIntent2;
     Intent myIntent3;
 
-    int lives;
-    int score;
+    int lives =3;
+    int score =0;
+    int level =1;
     int xPos =1;
     int imgInd =0;
+    int imgID = 100;
 
     int[] imgArr ={R.drawable.hearticon, R.drawable.dino};
 
@@ -58,8 +58,83 @@ public class GameActivity extends Activity {
         myIntent2 = getIntent();
         String username = myIntent2.getStringExtra("username");
         usernameTV.setText(username);
-        
+
+        livesTV.setText(Integer.toString(lives));
+
+        newImage(imgArr[imgInd], xPos, imgID);
+
+        timer = new CountDownTimer(3000,50) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                image.setVisibility(View.VISIBLE);
+                image.setY(image.getY() +50);
+            }
+
+            @Override
+            public void onFinish() {
+                changeImg();
+                timer.start();
+
+            }
+        }.start();
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.cancel();
+                image.setVisibility(View.GONE);
+                score += 1;
+                scoreTV.setText("Score: " + Integer.toString(score));
+                changeImg();
+                timer = new CountDownTimer(3000,50) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        image.setVisibility(View.VISIBLE);
+                        image.setY(image.getY() +50);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        changeImg();
+                        timer.start();
+
+                    }
+                }.start();
+
+            }
+        });
+
+        if (lives ==0) {
+            myIntent3 = new Intent(this, GameOverActivity.class);
+            myIntent3.putExtra("username", username);
+            myIntent3.putExtra("score", score);
+            myIntent3.putExtra("level", level);
+
+            GameActivity.this.startActivity(myIntent3);
+
+        }
+
     }
+
+    public void newImage(int img, int pos, int id) {
+        image = new ImageView(this);
+        image.setLayoutParams(new RelativeLayout.LayoutParams(200,200));
+        image.setId(id);
+        image.setY(0);
+        image.setX(pos * 100);
+        image.setImageResource(img);
+        gameSpace.addView(image);
+    }
+
+    public void changeImg() {
+        int totalX= (gameSpace.getWidth())-200;
+        imgInd = randNum.nextInt(2);
+        xPos = randNum.nextInt(totalX);
+        image.setImageResource(imgArr[imgInd]);
+        image.setY(0);
+        image.setX(xPos);
+    }
+
 
 
     @Override
--- app/src/main/java/com/example/kendra/groupd_finalproject/GameOverActivity.java
+++ app/src/main/java/com/example/kendra/groupd_finalproject/GameOverActivity.java
@@ -1,13 +1,26 @@
 package com.example.kendra.groupd_finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class GameOverActivity extends AppCompatActivity {
 
+    Intent myIntent4;
+
+    String username;
+    int score;
+    int level;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        myIntent4 = getIntent();
+        username = myIntent4.getStringExtra("username");
+        score = myIntent4.getIntExtra("score",0);
+        level = myIntent4.getIntExtra("level",0);
+
     }
 }
